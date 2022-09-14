25ecf63ce91e29dd6dc54324091c3bb9f8f79a4c
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-14 22:52:08 +0300

    Dispay contents

--- app.js
+++ app.js
@@ -10,8 +10,43 @@ class Book {
 // Storage class
 
 // UI class
+class UI {
+  static displaybooks () {
+    const locBooks =  [
+      {
+        title: 'River Between',
+        author: 'Wanaina',
+        category: 'old folks'
+      },
+      {
+        title: 'River Beyond',
+        author: 'Esther',
+        category: 'Comedy'
+      }
+    ]
+    const books = locBooks
+    books.forEach(book => UI.addbooktolist(book))
+  }
+
+  static addbooktolist(book) {
+    const unordered = document.querySelector('#unordered')
+    const listItem = document.createElement('li')
+    listItem.className = 'singleItem'
+    listItem.innerHTML = `
+      <div class="item">
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+        <p>${book.category}</p>
+        <button>X</button>
+      </div>
+    `
+
+    unordered.appendChild(listItem)
+  }
+}
 
 // Event to display Book
+document.addEventListener('DOMContentLoaded', UI.displaybooks)
 
 // Event: Add a Book
 
