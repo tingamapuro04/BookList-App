feeb2cd541bfa8fd557bb75b562894cd1e673af3
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-14 22:14:38 +0300

    Add Book class

---
+++ app.js
@@ -0,0 +1,18 @@
+// Book object
+class Book {
+  constructor(title, author, category) {
+    this.title = title;
+    this.author = author;
+    this.category = category
+  }
+}
+
+// Storage class
+
+// UI class
+
+// Event to display Book
+
+// Event: Add a Book
+
+// Event: Remove a book
